bd3c9f2fd2133f4db5829ef8f5f409ce191567f6
AndrewAndreichev <andrew1877718@gmail.com>
2020-09-19 19:33:38 +0300

    Add -A

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$('.navbar-collapse a').on('click', function () {
+  $(".navbar-collapse").collapse('hide');
+});
+
+$(window).scroll(function () {
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+});
+
+$(document).ready(function () {
+ 
+  $('.home-slider').owlCarousel({
+    animateOut: 'fadeOut',
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+  });
+
+  $('.owl-courses').owlCarousel({
+    animateOut: 'fadeOut',
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    dots: false,
+    smartSpeed: 1000,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle"></i>',
+      '<i class="fa fa-angle"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      }
+    }
+  });
+  
+
+  $('.owl-client').owlCarousel({
+    animateOut: 'fadeOut',
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      }
+    }
+  });
+
+});
